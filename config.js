942c0e78d09cb55df61717b84d90caa20164f81b
IDnets <baldibek.a0720@gmail.com>
2026-06-06 16:11:48 +0500

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Вставьте ваш Gemini API ключ сюда
-// Этот файл добавлен в .gitignore — ключ не попадёт в репозиторий
-window.GEMINI_API_KEY = 'AQ.Ab8RN6LFo8pLKEuwURQ-QI2YyIQAdv3Hc_idxQeHpH5PuMk-qg';
